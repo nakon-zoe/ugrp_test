6dd1d83037f52de20ec208b63e61b059d6c4bc5a
nakon-zoe <skrgjsdl23@gmail.com>
2020-10-31 00:03:02 +0900

    first

---
+++ calculator.py
@@ -0,0 +1,13 @@
+
+
+def calculate(num1, oper, num2):
+    result = None
+    if oper is '+':
+        result = num1 + num2
+    elif oper is '-':
+        result = num1 - num2
+    return result
+
+
+
+
